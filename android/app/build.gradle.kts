48fe7c706a937241e36c3caf1eb544ec947858c1
Ravi <ravi@gmail.com>
2025-11-02 21:35:30 +0530

    change the id and name of the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hostel_mess_bloc_version"
+    namespace = "org.gpbhojpur.hostel_mess_track"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hostel_mess_bloc_version"
+        applicationId = "org.gpbhojpur.hostel_mess"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
